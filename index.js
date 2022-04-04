ef46be767786f7602a727ddf011226623ac808b0
Kjell Maekelberg <67697869+KjellMeister-Severed@users.noreply.github.com>
2022-04-04 14:45:35 +0200

    add some demo endpoints

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const http = require('http');
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.status(404);
+})
+  
+
+app.get('/ping', (req, res) => {
+  res.send('Pong!');
 })
 
+app.post('/submit', (req, res) => {
+    res.send({
+        message: 'submission well received',
+        body: req.body
+    });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Spinaker Backend listening on port ${port}`)
+});
