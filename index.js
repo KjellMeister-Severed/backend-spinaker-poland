ff08b0f01962b04d7844e578e56073be96762a11
Kjell Maekelberg <67697869+KjellMeister-Severed@users.noreply.github.com>
2022-04-06 11:09:08 +0200

    non working db code example

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const http = require('http');
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = 3000
-
-app.get('/', (req, res) => {
-    res.status(404);
-})
-  
-
-app.get('/ping', (req, res) => {
-  res.send('Pong!');
+const express = require('express');
+const sql = require('mysql');
+const app = express();
+const port = 8080; // default port to listen
+const pool = sql.createPool({
+    connectionLimit: 10,
+    host: "s41.zenbox.pl",
+    port: 3306,
+    database: "ecoonomy_go22",
+    user: "ecoonomy_gopr",
+    password: "0hilFUj97W"
 })
 
-app.post('/submit', (req, res) => {
-    res.send({
-        message: 'submission well received',
-        body: req.body
-    });
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
 });
 
-app.listen(port, () => {
-  console.log(`Spinaker Backend listening on port ${port}`)
+app.get('/generate', (req, res) => {
+    pool.getConnection(function (err, connection) {
+        console.log(err)
+    })
+    res.send("generate")
+})
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
 });
