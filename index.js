339bb966811d2f8a13db53dfbb6c8ebeebf7df97
Kjell Maekelberg <67697869+KjellMeister-Severed@users.noreply.github.com>
2022-04-07 09:32:09 +0200

    add dynamic port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const generators = require('./generators');
 const caseManager = require('./case-manager')
 app.use(express.json());
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
 
 app.use(cors())
 
